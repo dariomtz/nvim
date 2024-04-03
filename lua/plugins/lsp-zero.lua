12fd89ef861885e4449149341708196f5a4206bb
Dario Martinez <dariom@lyft.com>
2024-04-02 19:58:27 -0600

    Remove dependencies and add git blame plugin

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,3 +1,11 @@
 return {
     'VonHeikemen/lsp-zero.nvim',
+    dependencies = {
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+    },
 }
